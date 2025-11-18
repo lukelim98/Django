5ad46cab7c70e3b1de2de613560aa6e111985dde
Joo Wan Lim <joowan980330@gmail.com>
2025-11-18 11:53:32 +0900

    Completed Section:6 Data & Models

--- BOOK_STORE/book_outlet/views.py
+++ BOOK_STORE/book_outlet/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from django.http import Http404
 from .models import Book
+from django.db.models import Avg, Max, Min
 
 # Create your views here.
 
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
+    num_books = books.count()
+    avg_rating = books.aggregate(avg_rating=Avg("rating"))
+
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books":num_books,
+        "average_rating":avg_rating
+    })
+
+def book_detail(request, slug):
+    try:
+        book = Book.objects.get(slug=slug)
+    except:
+        raise Http404()
+    return render(request, "book_outlet/book_detail.html", {
+        "title": book.title,
+        "author": book.author,
+        "rating": book.rating,
+        "is_best_seller": book.is_best_selling
     })
