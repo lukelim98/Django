45b98632f8387ca14ff8655190b4d0cd19204f06
Joo Wan Lim <joowan980330@gmail.com>
2025-11-20 16:52:14 +0900

    Added some models to my_site

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post, Author, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "date", "author", )
+    list_filter = ("author", "tags", "date", )
+    prepopulated_fields = {"slug": ("title",)}
 
 # Register your models here.
+admin.site.register(Post, PostAdmin)
+admin.site.register(Author)
+admin.site.register(Tag)
--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,86 +1,21 @@
 from datetime import date
 
 from django.shortcuts import render
+from .models import Post
 
-ALL_POSTS = [
-    {
-        "slug": "hike-in-the-mountains",
-        "image": "mountains.jpg",
-        "author": "Luke",
-        "date": date(2021, 7, 21),
-        "title": "Mountain Hiking",
-        "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared for what happened whilst I was enjoying the view!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Luke",
-        "date": date(2022, 3, 10),
-        "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    },
-    {
-        "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Luke",
-        "date": date(2020, 8, 5),
-        "title": "Nature At Its Best",
-        "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
-        "content": """
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
-          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
-          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
-        """
-    }
-]
 
 # Create your views here.
 
 
 def starting_page(request):
-    sorted_posts = sorted(ALL_POSTS, key=lambda x: x["date"], reverse=True)
-    latest_posts = sorted_posts[-3:]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, 'blog/index.html', {
         "posts": latest_posts
     })
 
 
 def posts(request):
-    sorted_posts = sorted(ALL_POSTS, key=lambda x: x['date'], reverse=True)
+    sorted_posts = Post.objects.all().order_by("-date")
     return render(request,
                 'blog/all-posts.html',
                 {
@@ -89,7 +24,7 @@ def posts(request):
 
 
 def post_detail(request, slug):
-    selected_post = next((post for post in ALL_POSTS if post['slug'] == slug), None)
+    selected_post = Post.objects.get(slug=slug)
     return render(request, "blog/post-detail.html",{
         "post":selected_post
     })
