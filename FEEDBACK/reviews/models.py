c758bbead7081d710615628b641d040b63d432e9
Joo Wan Lim <joowan980330@gmail.com>
2025-11-27 18:07:50 +0800

    Stored the from data in a database (model)

--- FEEDBACK/reviews/models.py
+++ FEEDBACK/reviews/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator, MaxLengthValidator
 
 # Create your models here.
+class Review(models.Model):
+    user_name = models.CharField(max_length=100)
+    review_text = models.TextField(validators=[MaxLengthValidator(200)])
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+
+    def __str__(self):
+        return f"{self.user_name}, {self.review_text}, {self.rating}"
