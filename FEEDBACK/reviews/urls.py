61bce4ff82358556568fc761a8084484c3f3c805
Joo Wan Lim <joowan980330@gmail.com>
2025-12-03 16:33:05 +0800

    Completed Section 13: Sessions

--- FEEDBACK/reviews/urls.py
+++ FEEDBACK/reviews/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.ReviewView.as_view()),
     path("thank-you", views.ThankYouView.as_view()),
     path("all-reviews", views.ReviewsView.as_view()),
+    path("all-reviews/favorite", views.AddFavoriteView.as_view()),
     path("all-reviews/<int:pk>", views.SingleReviewView.as_view(), name="single-review")
 ]
--- FEEDBACK/reviews/views.py
+++ FEEDBACK/reviews/views.py
@@ -96,5 +96,19 @@ class SingleReviewView(DetailView):
     template_name = "reviews/single_review.html"
     model = Review
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_review = self.object
+        request = self.request
+        favorite_id = request.session.get("favorite_review")
+        context["is_favorite"] = favorite_id == loaded_review.id
+        return context
+
 # def thank_you(request):
 #     return render(request, "reviews/thank_you.html")
+
+class AddFavoriteView(View):
+    def post(self, request):
+        review_id = request.POST['review_id']
+        request.session['favorite_review'] = int(review_id)
+        return HttpResponseRedirect("/all-reviews/" + review_id)
